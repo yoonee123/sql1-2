e30a1b4b3168a208d24e993392ea749a56431f8e
yoonee123 <78791337+yoonee123@users.noreply.github.com>
2023-10-16 10:58:12 +0900

    Delete 5주차과제_PA_20201599_정서윤 directory

--- 5주차과제_PA_20201599_정서윤/5-1.sql
+++
@@ -1,9 +0,0 @@
---1, 2번
-select top (6) with ties title_id, qty from sales order by qty desc 
---3, 4번
-select --distinct type 책분야 
-* from titles
-where type is not null
---5번
-select title + '('+ title_id+ ')' '책이름(책코드)' , price 
-from titles
--- 5주차과제_PA_20201599_정서윤/5-2.sql
+++
@@ -1,8 +0,0 @@
-select sum(가격)/count(*) 
-from 제품
-where 가격 is not null
-
-select sum(가격)/count(가격)
-from 제품
-
-select avg(가격) from 제품
